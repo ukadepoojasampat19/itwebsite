7fe41f7767c33570c2ee2f169202650354f82320
ukadepoojasampat19 <poojaukade8@gmail.com>
2024-02-21 22:51:23 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,14 @@
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.value.length >= 8){
+        alert("Login Succesfull");
+        window.location.replace("newpage.html");
+        return false;
+    }
+    else{
+        alert("Login Failed");
+    }
+}
